c9f2bc11f3b4fde1717e4e4169284bcc2d478b11
artur <artur.oliveirasantiago@gmail.com>
2021-04-06 22:28:02 -0300

    created header and footer structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProductSliderComponent } from './shared/product-slider/product-slider.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    ProductSliderComponent
+    ProductSliderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
