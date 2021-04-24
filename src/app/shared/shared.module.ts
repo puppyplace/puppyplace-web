f4f89b69819785971bf50483fea3f312872d48b7
Lucas Dias Lopes <lucas.fulldev@gmail.com>
2021-04-23 22:21:48 -0300

    add filtro de listagem de produtos

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,19 +4,22 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { PawComponent } from './paw/paw.component';
 import { ProductSliderComponent } from './product-slider/product-slider.component';
+import { ProductListManagerComponent } from './product-list-manager/product-list-manager.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     ProductSliderComponent,
     FooterComponent,
-    PawComponent
+    PawComponent,
+    ProductListManagerComponent
   ],
   exports: [
     HeaderComponent,
     ProductSliderComponent,
     FooterComponent,
-    PawComponent
+    PawComponent,
+    ProductListManagerComponent
   ],
   imports: [
     CommonModule
