4b05a052556b0e0d282901eedc148093d2f09f10
Beatriz Uezu <beatriz.uezu@gmail.com>
2021-06-16 20:56:02 -0300

    add firebase routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
+// import { LoginComponent } from './pages/login/login.component';
 
 import { AppComponent } from './app.component';
 import { CustomersComponent } from './pages/customers/customers.component';
 import { HomeComponent } from './pages/home/home.component';
-
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './pages/verify-email/verify-email.component';
 
 const routes: Routes = [
   {
@@ -13,10 +16,14 @@ const routes: Routes = [
     component: AppComponent,
     children: [
       { path: '', component: HomeComponent },
-      { path: 'login', component: LoginComponent },
+      // { path: 'login', component: LoginComponent },
       { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) },
       { path: 'sign-up', component: CustomersComponent },
-      { path: 'forgot-password', loadChildren: () => import('./pages/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule) }
+      { path: 'forgot-password', loadChildren: () => import('./pages/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule) },
+      { path: 'sign-in', component: SignInComponent },
+      { path: 'register-user', component: SignUpComponent },
+      { path: 'forgot-password', component: ForgotPasswordComponent },
+      { path: 'verify-email-address', component: VerifyEmailComponent }
     ]
   },
   {
