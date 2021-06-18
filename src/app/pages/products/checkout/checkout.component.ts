7f9f45d533540a761b90ac8003fdb6514bd5fe83
Lucas Dias Lopes <lucas.fulldev@gmail.com>
2021-06-18 20:51:15 -0300

    add checkout page

--- src/app/pages/products/checkout/checkout.component.ts
+++ src/app/pages/products/checkout/checkout.component.ts
@@ -1,42 +1,21 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
 import { ProductData } from 'src/app/models/carousel-data.interface';
 import { CartService } from 'src/app/shared/services/cart.service';
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.scss']
 })
-export class CartComponent implements OnInit, AfterViewInit {
-
-  @ViewChild('content') modal: ElementRef<NgbModal>;
+export class CheckoutComponent implements OnInit {
 
   public products: Array<ProductData>;
 
   constructor(
-    private cartService: CartService,
-    private modalService: NgbModal,
-    private router: Router) { }
-  
-  ngAfterViewInit(): void {
-    if (this.products.length === 0) {
-      this.open();
-    }
-  }
+    private cartService: CartService) { }
 
   ngOnInit(): void {
     this.products = this.cartService.getItems() || [];
-  }
-
-  open() {
-    this.modalService.open(
-      this.modal,
-      { ariaLabelledBy: 'modal-basic-title' }).dismissed.subscribe(res => {
-        this.router.navigate(['/products']);
-      })
-    
   }
 
   updateQtd(product: ProductData, raise: boolean, ev: Event) {
@@ -62,4 +41,5 @@ export class CartComponent implements OnInit, AfterViewInit {
   totalValue() {
     return this.products.reduce((curr, { total }) => curr + total, 0);
   }
+
 }
--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -6,12 +6,14 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductComponent } from './product/product.component';
 import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductComponent,
-    CartComponent
+    CartComponent,
+    CheckoutComponent
   ],
   imports: [
     CommonModule,
