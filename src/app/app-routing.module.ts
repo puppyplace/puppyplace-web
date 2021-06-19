cf3e39df8d774c2917b34d5b93a943725da8f748
Beatriz Uezu <beatriz.uezu@gmail.com>
2021-06-18 23:51:17 -0300

    comment routes to use firebase routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 // import { LoginComponent } from './pages/login/login.component';
 
 import { AppComponent } from './app.component';
-import { CustomersComponent } from './pages/customers/customers.component';
+// import { CustomersComponent } from './pages/customers/customers.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './pages/verify-email/verify-email.component';
+import { AuthGuard } from "./shared/guard/auth.guard";
 
 const routes: Routes = [
   {
@@ -18,8 +19,9 @@ const routes: Routes = [
       { path: '', component: HomeComponent },
       // { path: 'login', component: LoginComponent },
       { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) },
-      { path: 'sign-up', component: CustomersComponent },
-      { path: 'forgot-password', loadChildren: () => import('./pages/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule) },
+      // { path: 'sign-up', component: CustomersComponent },
+      // { path: 'forgot-password', loadChildren: () => import('./pages/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule) },
+      
       { path: 'sign-in', component: SignInComponent },
       { path: 'register-user', component: SignUpComponent },
       { path: 'forgot-password', component: ForgotPasswordComponent },
